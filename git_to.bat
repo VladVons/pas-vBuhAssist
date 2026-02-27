906725a46d9a42519e0e6d23899f0da5e20d5502
vladvons <vladvons@gmail.com>
2026-02-27 11:41:16 +0200

    q1

--- git_to.bat
+++ git_to.bat
@@ -7,6 +7,6 @@ git status
 git add .
 set /p msg="Enter commit message: "
 git commit -m "%msg%"
-rem git push origin main
-git push origin main --force
+git push origin main
+rem git push origin main --force
 pause
