ad6e621a69a44ea3f9e4207e7f3118b3a8e0c93f
vladvons <vladvons@gmail.com>
2026-03-03 09:52:05 +0200

    b38

--- src/vAppUpdProt.bat
+++ src/vAppUpdProt.bat
@@ -1,1 +1,8 @@
+set Ver=38
+set FileArch=vBuhAssist_%Ver%.exe.zip
+
+"C:\Program Files\Utils\upx.exe" -1 vBuhAssist.exe
 vAppUpd.exe --crc=vBuhAssist.exe
+"C:\Program Files\7-Zip\7z.exe" a %FileArch% vBuhAssist.exe
+"C:\Program Files\Utils\wput.exe" %FileArch% ftp://vladvons:19710819@oster.com.ua/www/download/public/update/vBuhAssist/
+del %FileArch%
