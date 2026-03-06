a8bd992966ee6da0398a84533ab1ee8bd93d1c0b
vladvons <vladvons@gmail.com>
2026-03-06 08:28:48 +0200

    q1

--- src/vBuhAssist_Release.bat
+++ src/vBuhAssist_Release.bat
@@ -1,21 +1,22 @@
 @echo off
-set PATH=C:\lazarus;C:\lazarus\fpc\3.2.2\bin\i386-win32;%PATH%
 
+rem set BuildMode=Release
+set BuildMode=Default
+
+set PATH=C:\lazarus;C:\lazarus\fpc\3.2.2\bin\i386-win32;C:\Program Files\Utils;C:\Program Files\7-Zip;%PATH%
 set App=vBuhAssist
-
 RMDIR /S /Q _lib
 call %App%_Rc.bat
-lazbuild %App%.lpi
-rem lazbuild vBuhAssist.lpi --build-mode=Release
+lazbuild %App%.lpi --build-mode=%BuildMode%
 
 set FileExe=%App%.exe
 vAppUpd.exe --app_build=%FileExe% > %App%_ver.txt
 set /p BUILD=<%App%_ver.txt
 set FileArch=%App%_%BUILD%.exe.zip
 
-"C:\Program Files\Utils\upx.exe" -5 %FileExe%
+upx.exe -5 %FileExe%
 vAppUpd.exe --crc=%FileExe%
-"C:\Program Files\7-Zip\7z.exe" a %FileArch% %FileExe%
-"C:\Program Files\Utils\wput.exe" %FileArch% ftp://vladvons:19710819@oster.com.ua/www/download/public/update/vBuhAssist/
+7z.exe a %FileArch% %FileExe%
+rem wput.exe %FileArch% ftp://vladvons:19710819@oster.com.ua/www/download/public/update/vBuhAssist/
 rem del %FileArch%
 del %FileExe%
