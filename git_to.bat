3499efa64d7a6ae5e397275b4d7916851f320633
vladvons <vladvons@gmail.com>
2026-03-31 08:30:09 +0300

    q1

--- git_to.bat
+++ git_to.bat
@@ -3,7 +3,7 @@ rem git config --global user.email "vladvons@gmail.com"
 rem git config --global user.name "vladvons"
 
 rem git rm --cached vAppUpd.log
-rem git rm --cached -r *.pdf
+rem git rm --cached -r *.res
 rem git rm --cached -r junk
 
 git status
