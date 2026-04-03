c7b2e4c1a15cfde13dee41493443c6a920434b2b
vladvons <vladvons@gmail.com>
2026-04-03 23:32:09 +0300

    q1

--- git_to.bat
+++ git_to.bat
@@ -4,12 +4,13 @@ rem git config --global user.name "vladvons"
 
 rem git rm --cached vAppUpd.log
 rem git rm --cached -r *.res
-git rm --cached -r Data
+rem git rm --cached -r src/Data
+rem git rm --cached -r src/Junk
 
 git status
 git add .
 set /p msg="Enter commit message: "
 git commit -m "%msg%"
-rem git push origin main
+git push origin main
 rem git push origin main --force
 pause
